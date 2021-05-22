f49bd5560c09ba7b259dad0208953dd038d83a14
xiexuezhen <309114330@qq.com>
2021-05-22 22:20:20 +0800

    feat: 增加了注册功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
-from . import auth
-from ..models import User
-from .forms import LoginForm
+from app.auth import auth
+from app import db
+from app.models import User
+from app.auth.forms import LoginForm
+from app.auth.forms import RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -26,3 +28,17 @@ def logout():
     logout_user()
     flash('You have been logged out')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data.lower(),
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
