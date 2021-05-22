e4ff0a0b619019c5425ff2fbb4d4b90a2ce4b3ec
xiexuezhen <309114330@qq.com>
2021-05-22 23:19:53 +0800

    feat: 增加了确认用户账户功能

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from . import login_manager
-from . import db
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from app import login_manager
+from app import db
 
 
 class Role(db.Model):
@@ -21,6 +23,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -35,6 +38,22 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
 
 @login_manager.user_loader
