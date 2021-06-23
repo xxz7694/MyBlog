890493b18f7fff516f8b50951e7fb8efcecb7fab
xiexuezhen <309114330@qq.com>
2021-06-23 23:49:30 +0800

    feat: 实现用户的关注功能

--- app/models.py
+++ app/models.py
@@ -116,6 +116,7 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = self.gravatar_hash()
+        self.follow(self)
 
     @property
     def password(self):
@@ -226,6 +227,18 @@ class User(UserMixin, db.Model):
             return False
         return self.followed.filter_by(followed_id=user.id).first() is not None
 
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
+            .filter(Follow.follower_id == self.id)
+
+    @staticmethod
+    def add_self_follow():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
